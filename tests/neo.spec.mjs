269967c549ff3d7bdffc53a5e0192ef8fcb47724
tobiu <tobiasuhlig78@gmail.com>
2025-08-07 21:17:26 +0200

    using playwright with mjs files (instead of commonJS)

--- tests/neo.spec.mjs
+++ tests/neo.spec.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 
 test('Neo.mjs benchmark: Create 1k rows', async ({ page }) => {
   await page.goto('/apps/benchmarks/');
